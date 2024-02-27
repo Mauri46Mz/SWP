8d50603b8669e0b9206f9207e0b6b65e70e6733e
Mauri <mauriziomozzo46@gmail.com>
2024-02-27 14:27:49 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,15 @@
     let promise = supabase.from('countries').select();
 </script>
 
+<h1>Aufgaben</h1>
+<ul>
+    <li>anstatt eines Js-Objekts, das auf der Seite dargestellt wird, möchte ich eine Liste aller Länder</li>
+    <li>integriere einen loading spinner oÄ von daisyUI </li>
+    <li>gib die Vercel URL zu deinem Projekt via Aufgabe in Teams ab</li>
+</ul>
+
 {#await promise}
-<div>Loading...</div> 
+    <div>Loading...</div> 
 {:then result} 
-<div>{JSON.stringify(result)}</div>
+    <div>{JSON.stringify(result)}</div>
 {/await}
